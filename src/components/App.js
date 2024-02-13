af370ceaf1118f14bdf1ed8e3190bddc45f4a5b4
Dipak1121 <dipaks9604@gmail.com>
2024-02-13 11:43:15 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,53 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, {useState} from "react";
+import data from "../Data/data";
+
+
+const App = ()=>{
+
+    const [currentReview, setCurrentReview] = useState(data[0]);
+
+    const [currentId, setCurrentId] = useState(0);
+
+    function handlePrevious(){
+        if(currentId == 0){
+            return;
+        }
+        let id = currentId;
+        setCurrentId(currentId-1);
+        setCurrentReview(data[id-1]);
+    }
+
+    function handleNext(){
+        if(currentId == 3){
+            return;
+        }
+        let id = currentId;
+        setCurrentId(currentId+1);
+        setCurrentReview(data[id+1]);
+    }
+
+
+    function handleRandom(){
+        setCurrentId(0);
+        setCurrentReview(data[0]);
+    }
+    return(
+        <div>
+            <h1 id="review-heading">Our Reviews</h1>
+            <div className="review">
+                <h3 className="author" id={currentReview.id}>{currentReview.name}</h3>
+                <h4 className="job">{currentReview.job}</h4>
+                <p className="info">{currentReview.text}</p>
+                <img className="person-img" src={currentReview.image}/>
+            </div>
+            <div>
+                <button className="prev-btn" onClick={handlePrevious}>Previous</button>
+                <button className="next-btn" onClick={handleNext}>Next</button>
+                <button className="random-btn" onClick={handleRandom}>Random Review</button>
+            </div>
+        </div>
+    )
+}
+
+export default App;
 
